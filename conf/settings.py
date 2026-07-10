e2aef9455add6d097f4cfb2301817e6fa9dcdd2c
Amol C <amolch001@gmail.com>
2026-07-10 16:05:13 +0530

    added proper requirements.txt

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4s(1eyu@+&w!=1-_j6asn!)7nli8q39h17u6ze4vjoir)+m9%u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','pipeline.quantbots.co']
 
 
 # Application definition
